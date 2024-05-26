19dd6a1987671eed732143feed6d372ccf5ee79d
sept1st2c <sept1st2c@gmail.com>
2024-05-27 03:48:34 +0530

    sunday

--- week-3/04-mongo-with-jwt-auth/solution/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/solution/routes/user.js
@@ -1,29 +1,41 @@
-const { Router } = require("express");
-const router = Router();
 const userMiddleware = require("../middleware/user");
+const { Router } = require("express");
+const { Admin, User, Course } = require("../db");
+const { JWT_SECRET } = require("../config");
+const router = Router();
+const jwt = require("jsonwebtoken");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  await User.create({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "Admin created successfully",
+  });
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", (req, res) => {
+  // Implement admin signup logic
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", (req, res) => {
+  // Implement listing all courses logic
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
-    const username = req.username;
-    console.log(username);
-
+router.post("/courses/:courseId", userMiddleware, (req, res) => {
+  // Implement course purchase logic
+  const username = req.username;
+  console.log(username);
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, (req, res) => {
+  // Implement fetching purchased courses logic
 });
 
-module.exports = router
+module.exports = router;
